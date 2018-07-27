63d18ec50f8eacba1861a5270b7403e414c7fe19
Vitaliy <theyamshikov@yandex.ru>
2018-07-28 00:55:29 +0600

    refacto search

--- src/activeRecord/helpers/SearchHelper.php
+++ src/activeRecord/helpers/SearchHelper.php
@@ -2,10 +2,10 @@
 
 namespace yii2lab\extension\activeRecord\helpers;
 
+use Yii;
 use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
 use yii2lab\domain\data\Query;
-use yii2lab\domain\exceptions\UnprocessableEntityHttpException;
-use yii2lab\domain\helpers\ErrorCollection;
 
 class SearchHelper {
 	
@@ -14,16 +14,12 @@ class SearchHelper {
 	
 	public static function extractSearchTextFromQuery(Query $query) {
 		$searchText = $query->getWhere(self::SEARCH_PARAM_NAME);
-		if(empty($searchText)) {
-			return null;
-		}
 		$query->removeWhere(self::SEARCH_PARAM_NAME);
 		$searchText = trim($searchText);
 		return $searchText;
 	}
 	
 	public static function appendSearchCondition(Query $query, $searchByTextFields, $searchText) {
-		//$searchText = self::extractSearchTextFromQuery($query);
 		self::validateSearchText($searchText);
 		$likeCondition = self::generateLikeCondition($searchText, $searchByTextFields);
 		$query->andWhere($likeCondition);
@@ -43,12 +39,10 @@ class SearchHelper {
 	private static function validateSearchText($text) {
 		$text = trim($text);
 		if(empty($text) || mb_strlen($text) < self::SEARCH_TEXT_MIN_LENGTH) {
-			$error = new ErrorCollection;
-			$error->add('text', 'yii', '{attribute} should contain at least {min, number} {min, plural, one{character} other{characters}}.', [
-				'attribute'=>'text',
+			throw new BadRequestHttpException(Yii::t('yii', '{attribute} should contain at least {min, number} {min, plural, one{character} other{characters}}.', [
+				'attribute'=>self::SEARCH_PARAM_NAME,
 				'min'=>self::SEARCH_TEXT_MIN_LENGTH,
-			]);
-			throw new UnprocessableEntityHttpException($error);
+			]));
 		}
 	}
 	

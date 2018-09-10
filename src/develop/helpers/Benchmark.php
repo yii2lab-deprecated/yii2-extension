1cd347482d00070ef75b53e1232852021cd64148
Vitaliy <theyamshikov@yandex.ru>
2018-09-10 18:37:54 +0600

    update benchmark

--- src/develop/helpers/Benchmark.php
+++ src/develop/helpers/Benchmark.php
@@ -1,32 +1,84 @@
 <?php
 
-namespace yii2lab\misc\exception\develop\helpers;
+namespace yii2lab\extension\develop\helpers;
 
 use yii\web\ServerErrorHttpException;
+use yii2lab\app\domain\helpers\EnvService;
+use yii2lab\extension\store\StoreFile;
+use yii2lab\helpers\StringHelper;
 
 class Benchmark {
 	
 	private static $data = [];
+	private static $sessionId = null;
 	
-	public static function begin($name) {
-		self::$data[$name]['begin'] = microtime(true);
-	}
-	
-	public static function end($name) {
-		if(isset(self::$data[$name]['end'])) {
+	public static function begin($name, $data = null) {
+		if(!self::isEnable()) {
 			return;
 		}
-		if(!isset(self::$data[$name]['begin'])) {
-			throw new ServerErrorHttpException('Benchmark not be started!');
-		}
-		self::$data[$name]['end'] = microtime(true);
+		$item['name'] = $name;
+		$item['begin'] = microtime(true);
+		$item['data'] = [$data];
+		self::append($item);
 	}
 	
-	public static function prr($name) {
-		self::end($name);
+	public static function end($name, $data = null) {
+		if(!self::isEnable()) {
+			return;
+		}
+		if(!isset(self::$data[$name])) {
+			return;
+		}
 		$item = self::$data[$name];
-		$duration = $item['end'] - $item['begin'];
-		prr('Duration: ' . $duration,1);
+		if(isset($item['end'])) {
+			return;
+		}
+		
+		if(!isset($item['begin'])) {
+			throw new ServerErrorHttpException('Benchmark not be started!');
+		}
+		$item['end'] = microtime(true);
+		if($data) {
+			$item['data'][] = $data;
+		}
+		self::append($item);
+	}
+	
+	public static function all() {
+		return self::$data;
+	}
+	
+	private static function isEnable() {
+		return EnvService::get('mode.benchmark', false);
+	}
+	
+	private static function getRequestId() {
+		if(!self::$sessionId) {
+			self::$sessionId = TIMESTAMP . DOT . StringHelper::generateRandomString();
+		}
+		return self::$sessionId;
+	}
+	
+	private static function getFileName() {
+		$dir = \Yii::getAlias('@common/runtime/logs/benchmark');
+		$file = self::getRequestId() . '.json';
+		return $dir . DS . $file;
+	}
+	
+	private static function getStoreInstance() {
+		$fileName = self::getFileName();
+		$store = new StoreFile($fileName);
+		return $store;
+	}
+	
+	private static function append($item) {
+		$name = $item['name'];
+		self::$data[$name] = $item;
+		if(!empty($item['end'])) {
+			$item['duration'] = $item['end'] - $item['begin'];
+			$store = self::getStoreInstance();
+			$store->save(self::$data);
+		}
 	}
 	
 }

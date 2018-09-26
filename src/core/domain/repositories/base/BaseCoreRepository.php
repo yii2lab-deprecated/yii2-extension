72842ab51b4a3097152a0195245fe03de711d719
Vitaliy <theyamshikov@yandex.ru>
2018-09-26 11:08:35 +0600

    fix

--- src/core/domain/repositories/base/BaseCoreRepository.php
+++ src/core/domain/repositories/base/BaseCoreRepository.php
@@ -49,7 +49,7 @@ class BaseCoreRepository extends BaseRestRepository {
 			throw new UnprocessableEntityHttpException($responseEntity->data);
 		}
 		if($statusCode == 401) {
-			\App::$domain->account->auth->breakSession();
+			\App::$domain->account->auth->loginRequired();
 			return;
 		}
 		parent::showUserException($responseEntity);

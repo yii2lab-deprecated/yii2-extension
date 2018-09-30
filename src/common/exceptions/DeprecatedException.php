a29aed663d75dfe0ab40bc2b9ee6c9805246fb2e
Vitaliy <theyamshikov@yandex.ru>
2018-09-30 19:15:16 +0600

    clean

--- src/common/exceptions/DeprecatedException.php
+++ src/common/exceptions/DeprecatedException.php
@@ -5,4 +5,8 @@ namespace yii2lab\extension\common\exceptions;
 use yii\base\Exception;
 
 class DeprecatedException extends Exception {
+	
+	public function getName() {
+		return 'DeprecatedException';
+	}
 }

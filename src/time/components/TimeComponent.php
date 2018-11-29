2a19b68d8b7241e5d826ca4b3c006af25227f4e6
Vitaliy <theyamshikov@yandex.ru>
2018-11-29 12:48:55 +0600

    fix

--- src/time/components/TimeComponent.php
+++ src/time/components/TimeComponent.php
@@ -8,6 +8,9 @@ use yii2lab\extension\web\helpers\ControllerHelper;
 class TimeComponent extends Validator {
 	
 	public function init() {
+		if(APP == CONSOLE) {
+			return;
+		}
 		$timeZone = ControllerHelper::setTimeZone();
 		if($timeZone != \Yii::$app->timeZone) {
 			\Yii::$app->timeZone = $timeZone;

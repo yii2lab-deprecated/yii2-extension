4bcfd31495e3c69ec33429d9a247f420cf2709f8
amubarak <amubarak@wooppay.com>
2019-04-03 11:56:03 +0600

    cahce

---
+++ src/cache/repositories/base/BaseActiveCacheRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace yii2lab\extension\cache\repositories\base;
+
+use yii2lab\domain\interfaces\repositories\CrudInterface;
+use yii2lab\extension\activeRecord\repositories\base\BaseActiveArRepository;
+
+abstract class BaseActiveCacheRepository extends BaseActiveArRepository implements CrudInterface {
+
+}
---
+++ src/cache/repositories/base/BaseCacheRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace yii2lab\extension\cache\repositories\base;
+
+use yii2lab\extension\activeRecord\repositories\base\BaseArRepository;
+
+abstract class BaseCacheRepository extends BaseArRepository {
+
+}

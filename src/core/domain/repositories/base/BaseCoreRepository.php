a8c3f3956245278dfd9fddb812df66a6cf71bccb
amubarak <amubarak@wooppay.com>
2019-04-19 16:46:23 +0600

    set warning

--- src/core/domain/repositories/base/BaseCoreRepository.php
+++ src/core/domain/repositories/base/BaseCoreRepository.php
@@ -30,13 +30,8 @@ class BaseCoreRepository extends BaseRestRepository
 	protected function sendRequest(RequestEntity $requestEntity)
 	{
 		$headers = $requestEntity->headers;
-		if (!empty($headers[ClientHelper::IP_HEADER_KEY])) {
-			if (empty(Yii::$app->request->headers->get('partner-name'))) {
-				$headers[ClientHelper::IP_HEADER_KEY] = ClientHelper::getIpFromRequest();
-			}
-		} else {
-			$headers[ClientHelper::IP_HEADER_KEY] = ClientHelper::getIpFromRequest();
-		};
+		$headers[ClientHelper::IP_HEADER_KEY] = ClientHelper::getIpFromRequest();
+		Yii::warning($headers[ClientHelper::IP_HEADER_KEY], __METHOD__);
 		$requestEntity->headers = $headers;
 		$responseEntity = parent::sendRequest($requestEntity);
 		return $responseEntity;

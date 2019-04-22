e5b5b41f2531cf38673795484292adaaace51913
amubarak <amubarak@wooppay.com>
2019-04-22 13:47:18 +0600

    f

--- src/web/helpers/ClientHelper.php
+++ src/web/helpers/ClientHelper.php
@@ -114,10 +114,6 @@ class ClientHelper {
 			return self::LOCALHOST_IP;
 		}
 		$ip = Yii::$app->request->headers->get(self::IP_HEADER_KEY, false);
-		if(empty($ip)) {
-			prr('big_problem', 1, 1);
-		}
-		
 		return $ip;
 	}
 	

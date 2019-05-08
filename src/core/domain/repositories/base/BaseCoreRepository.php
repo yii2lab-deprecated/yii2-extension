a279216ecb6d1a6a24301e92c1e19a48b867e87e
amubarak <amubarak@wooppay.com>
2019-05-08 12:39:13 +0600

    remove

--- src/core/domain/repositories/base/BaseCoreRepository.php
+++ src/core/domain/repositories/base/BaseCoreRepository.php
@@ -33,7 +33,6 @@ class BaseCoreRepository extends BaseRestRepository
 		$headers[ClientHelper::IP_HEADER_KEY] = ClientHelper::getIpFromRequest();
 		$requestEntity->headers = $headers;
 		$responseEntity = parent::sendRequest($requestEntity);
-		prr($responseEntity->data,1,1);
 		return $responseEntity;
 	}
 
